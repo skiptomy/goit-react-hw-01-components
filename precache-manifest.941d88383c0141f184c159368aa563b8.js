57945b055ee45a74277ce311f286a5c0bcc1c6d8
Roman Lysenko <skiptomy55@gmail.com>
2020-01-22 22:37:53 +0000

    Updates

---
+++ precache-manifest.941d88383c0141f184c159368aa563b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01cba1b77ac50265b713510560ccc2a4",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "0ea9408f8d954f065cf6",
+    "url": "/goit-react-hw-01-components/static/css/main.28061673.chunk.css"
+  },
+  {
+    "revision": "d02421a02979b928738e",
+    "url": "/goit-react-hw-01-components/static/js/2.600d39c2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-01-components/static/js/2.600d39c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ea9408f8d954f065cf6",
+    "url": "/goit-react-hw-01-components/static/js/main.6f6166b4.chunk.js"
+  },
+  {
+    "revision": "5529903743abc23e6f33",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+  }
+]);
